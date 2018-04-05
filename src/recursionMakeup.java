ad91cb05b19f67558c10631c2f25e48f875f831a
Aidan <Aidan@Aidan-PC>
2018-04-04 22:49:39 -0400

    Liking this first method, debugging and testing time Just saw Ready Player One, pretty good movie, got pumped and wrote this method

--- src/recursionMakeup.java
+++ src/recursionMakeup.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class recursionMakeup {
 
@@ -36,94 +37,107 @@ public class recursionMakeup {
 		    }	
 	    }
 	    
-	    int l2 = (int)(Math.random() * ((l)));
-	    int w2 = (int)(Math.random() * ((w)));
+	    int l2 = (int)(Math.random() * l);
+	    int w2 = (int)(Math.random() * w);
+	    
+	    Stack<Integer> stack = new Stack<Integer>();
 	    
 	    display(array);
 	    System.out.println(l2);
 	    System.out.println(w2);
+	    System.out.println(array[l2][w2] + "\n");
 	    
-	    
-	    System.out.println(longestPathEasy(l2, w2, array, array[l2][w2], 0));
+	    System.out.println(longestPathEasy(l2, w2, array, array[l2][w2], stack));
 	    
 	    //System.out.println(longestPath(l2, w2, array, array[l2][w2], 0, 0));
 	    
 	}
 
-
-	private static int longestPathEasy(int l, int w, int[][] array, int num, int c) {
-		
-		if(array[1][w] != num) {
-			return -1;
-		}
-		c++;
-		
-		
-		System.out.println("[" + l + "] [" + w + "]");
-		
-		boolean r = false;
-		boolean d = false;
-
-		//right
-		if(w <= array[0].length && array[l][w+1] != -1) {
-			r = true;
-		}
+	
+	private static Stack<Integer> longestPathEasy(int l, int w, int[][] array, int num, Stack<Integer> stack) {
 		//down
-		if(l <= array.length || array[l+1][w] != -1) {
-			d = true;
-		}
-		
-		if(r&&d) {
-			//longpath1
-			//longpath2
-		}else { 
-			if(r){
-				//longpath1
-			}
-			if(d){
-				//longpath2
-			}
-			System.out.println(c);
-			
-		}
-
-		
-		
-		
-		return 0;
-	}
-
-
-	private static int longestPath(int l, int w, int[][] array, int num, int last, int c) {
-		
-		if(array[1][w] != num) {
-			return -1;
-		}
-		c++;
-		//make sure we dont go same path
-		
-		
-		
-		//up
-		if(l != 0 || array[l][w] != -1) {
-			
-		}
-		//left
-		if(array[l][w] != -1) {
-			
+		if(l > array[0].length && array[1+1][w] == num && array[1][w+1] != num) {
+			stack.push(l+1);
+			stack.push(w);
+			return longestPathEasy(l+1, w, array, num, stack);
 		}
 		//right
-		if(array[l][w] != -1) {
+		if(w > array.length && array[1][w+1] == num && array[1+1][w] != num) {
+			stack.push(l);
+			stack.push(w+1);
+			return longestPathEasy(l, w+1, array, num, stack);
+		}
+		//both
+		if(array[1][w+1] == num && array[1+1][w] == num) {
+			//if down stack bigger add and return down stack
+			if(longestPathEasy(l+1, w, array, num, stack).size() > longestPathEasy(l, w+1, array, num, stack).size()) {
+				stack.push(l+1);
+				stack.push(w);
+				return longestPathEasy(l+1, w, array, num, stack);
+			//if right stack bigger add and return right stack
+			}else{
+				stack.push(l);
+				stack.push(w+1);
+				return longestPathEasy(l, w+1, array, num, stack);
+			}
 			
 		}
-		//down
-		if(array[l][w] != -1) {
-			
-		}
-		
-		
-		
-		return 0;
+		//neither
+		return stack;
 	}
+	
+	
+	
+	
+	
+
+//	private static Stack<Integer> longestPath(int l, int w, int[][] array, int num, Stack<Integer> stack) {
+//		
+//		if(array[1][w] != num) {
+//			return null;
+//		}
+//		
+//		
+//		
+//		//System.out.println("[" + l + "] [" + w + "]");
+//		
+//		boolean r = false;
+//		boolean d = false;
+//
+//		//right
+//		if(w <= array[0].length && array[l][w+1] == num) {
+//			r = true;
+//		}
+//		//down
+//		if(l <= array.length || array[l+1][w] == num) {
+//			d = true;
+//		}
+//		
+//		if(r&&d) {
+//			if(longestPathEasy(l+1, w, array, num, stack) != null) {
+//				
+//			}
+//			if(longestPathEasy(l, w+1, array, num, stack) != null) {
+//				
+//			}
+//		}else { 
+//			if(r){
+//				//longpath1
+//			}
+//			if(d){
+//				//longpath2
+//			}
+//			System.out.println(c);
+//			
+//		}
+//
+//		
+//		
+//		
+//		return 0;
+//	}
+
+
+	
 
 }
