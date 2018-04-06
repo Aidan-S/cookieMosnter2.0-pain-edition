3b7456ca0b9dfe5e6c01e3cd5f372312a6ecda35
Aidan <Aidan@Aidan-PC>
2018-04-06 18:28:28 -0400

    havn't pushed in a while but was working out annoying bugs, who needs sleep

--- src/recursionMakeup.java
+++ src/recursionMakeup.java
@@ -37,17 +37,35 @@ public class recursionMakeup {
 		    }	
 	    }
 	    
-	    int l2 = (int)(Math.random() * l);
-	    int w2 = (int)(Math.random() * w);
+	    //int l2 = (int)(Math.random() * l);
+	    //int w2 = (int)(Math.random() * w);
 	    
 	    Stack<Integer> stack = new Stack<Integer>();
 	    
 	    display(array);
-	    System.out.println(l2);
-	    System.out.println(w2);
+	    
+	    //System.out.println(l2);
+	    //System.out.println(w2);
+	    
+	    System.out.print("Length: ");
+	    int l2 = kb.nextInt();
+	    System.out.print("Width: ");
+	    int w2 = kb.nextInt();
+	    
 	    System.out.println(array[l2][w2] + "\n");
 	    
-	    System.out.println(longestPathEasy(l2, w2, array, array[l2][w2], stack));
+	    
+	    stack = longestPathEasy(l2, w2, array, array[l2][w2], stack);
+	    
+	    int s = stack.size()/2;
+	    
+	    String str = "";
+	    for(int i = 0; i < s; i++) {
+	    	str = "[" + stack.pop() + ", " + stack.pop() + "]  " + str;
+	    }
+	    System.out.println("[" + l2 + ", " + w2 + "]  " + str);
+	    
+	    //System.out.println(longestPathEasy(l2, w2, array, array[l2][w2], stack));
 	    
 	    //System.out.println(longestPath(l2, w2, array, array[l2][w2], 0, 0));
 	    
@@ -55,35 +73,43 @@ public class recursionMakeup {
 
 	
 	private static Stack<Integer> longestPathEasy(int l, int w, int[][] array, int num, Stack<Integer> stack) {
+		boolean r = ( w+1 < array[0].length && array[l][w+1] == num );
+		boolean d = ( l+1 < array.length && array[l+1][w] == num );
+		Stack<Integer> newStack = new Stack<Integer>(); 
+		
 		//down
-		if(l > array[0].length && array[1+1][w] == num && array[1][w+1] != num) {
-			stack.push(l+1);
-			stack.push(w);
-			return longestPathEasy(l+1, w, array, num, stack);
-		}
-		//right
-		if(w > array.length && array[1][w+1] == num && array[1+1][w] != num) {
-			stack.push(l);
-			stack.push(w+1);
-			return longestPathEasy(l, w+1, array, num, stack);
-		}
-		//both
-		if(array[1][w+1] == num && array[1+1][w] == num) {
-			//if down stack bigger add and return down stack
-			if(longestPathEasy(l+1, w, array, num, stack).size() > longestPathEasy(l, w+1, array, num, stack).size()) {
-				stack.push(l+1);
-				stack.push(w);
-				return longestPathEasy(l+1, w, array, num, stack);
-			//if right stack bigger add and return right stack
+		if(d && !r) {
+			newStack = stack;
+			newStack.push(w);
+			newStack.push(l+1);
+			return longestPathEasy(l+1, w, array, num, newStack);
+		}else{
+			//right
+			if(r && !d) {
+				newStack = stack;
+				newStack.push(w+1);
+				newStack.push(l);
+				return longestPathEasy(l, w+1, array, num, newStack);
 			}else{
-				stack.push(l);
-				stack.push(w+1);
-				return longestPathEasy(l, w+1, array, num, stack);
+				//both
+				if(d && r) {
+					if(longestPathEasy(l+1, w, array, num, newStack).size() > longestPathEasy(l, w+1, array, num, newStack).size()) {
+						newStack = stack;
+						newStack.push(w);
+						newStack.push(l+1);
+						return longestPathEasy(l+1, w, array, num, newStack);
+					}else{
+						newStack = stack;
+						newStack.push(w+1);
+						newStack.push(l);
+						return longestPathEasy(l, w+1, array, num, newStack);
+					}
+				}else{
+					//neither
+					return stack;
+				}		
 			}
-			
-		}
-		//neither
-		return stack;
+		}	
 	}
 	
 	
@@ -91,51 +117,11 @@ public class recursionMakeup {
 	
 	
 
-//	private static Stack<Integer> longestPath(int l, int w, int[][] array, int num, Stack<Integer> stack) {
-//		
-//		if(array[1][w] != num) {
-//			return null;
-//		}
-//		
-//		
-//		
-//		//System.out.println("[" + l + "] [" + w + "]");
-//		
-//		boolean r = false;
-//		boolean d = false;
-//
-//		//right
-//		if(w <= array[0].length && array[l][w+1] == num) {
-//			r = true;
-//		}
-//		//down
-//		if(l <= array.length || array[l+1][w] == num) {
-//			d = true;
-//		}
-//		
-//		if(r&&d) {
-//			if(longestPathEasy(l+1, w, array, num, stack) != null) {
-//				
-//			}
-//			if(longestPathEasy(l, w+1, array, num, stack) != null) {
-//				
-//			}
-//		}else { 
-//			if(r){
-//				//longpath1
-//			}
-//			if(d){
-//				//longpath2
-//			}
-//			System.out.println(c);
-//			
-//		}
-//
-//		
-//		
-//		
-//		return 0;
-//	}
+	private static Stack<Integer> longestPath(int l, int w, int[][] array, int num, Stack<Integer> stack) {
+		
+		
+		return null;
+	}
 
 
 	
